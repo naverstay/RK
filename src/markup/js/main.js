85e7589737b91f5fa23ef126f6adb25c9c84d1d2
naverstay <naverstay.me@gmail.com>
2017-06-23 10:58:13 +0300

    index release

--- src/markup/js/main.js
+++ src/markup/js/main.js
@@ -1,18 +1,57 @@
 /*результат генерации bower Js*/
 /* //= libs/_vendor.js    - если не хочетс подключать 2 файла, можно всё объединить */
 
-var body, heroSwiper;
+var body, heroSwiper, menuSlider;
 
 $(function () {
 
   body = $('body');
 
   body
+    .delegate('.valPlus', 'click', function () {
+      var valCell = $(this).closest('.valCell'),
+        inp = valCell.find('input'),
+        val = parseInt(inp.val().replace(/\D/g, '')),
+        min_val = 1 * inp.attr('data-min'),
+        max_val = 1 * inp.attr('data-max'),
+        new_val = val + (1 * inp.attr('data-step'));
+
+      inp.val(new_val <= max_val ? (new_val >= min_val ? new_val : min_val) : max_val).focus();
+
+      return false;
+    })
+    .delegate('.valMinus', 'click', function () {
+      var valCell = $(this).closest('.valCell'),
+        inp = valCell.find('input'),
+        val = parseInt(inp.val().replace(/\D/g, '')),
+        min_val = 1 * inp.attr('data-min'),
+        max_val = 1 * inp.attr('data-max'),
+        new_val = val - (1 * inp.attr('data-step'));
+
+      inp.val(new_val >= min_val ? (new_val <= max_val ? new_val : max_val) : min_val).focus();
+
+      return false;
+    })
     .delegate('.dropdownBtn', 'click', function () {
       var firedEl = $(this);
       firedEl.parent().toggleClass('opened');
 
       toggleOverlay(firedEl.parent(), firedEl.parent().hasClass('opened'));
+
+      return false;
+    })
+    .delegate('.triggerBtn', 'click', function () {
+      var btn = $(this), list = btn.closest('.triggerList');
+
+      if (btn.hasClass('_asc')) {
+        btn.removeClass('_asc').addClass('_desc');
+      } else if (btn.hasClass('_desc')) {
+        btn.removeClass('_desc').addClass('_asc');
+      } else {
+        btn.addClass('_desc');
+      }
+
+      list.find('.triggerBtn').not(btn).removeClass('_desc').removeClass('_ssc');
 
       return false;
     })
@@ -29,8 +68,6 @@ $(function () {
     .delegate('.openSubmenu', 'click', function () {
       var link = $(this), menu = link.next('ul');
 
-      console.log(menu.html());
-      
       $('.subNavMenu').html(menu.html());
       $('.subNavCaption').text(link.text());
       $('.subNav').toggleClass('subnav_opened');
@@ -70,7 +107,42 @@ $(function () {
 
   initHeroSlider();
 
+  initMenuSlider();
+
+  initMask();
+
 });
+
+function initMask(el) {
+
+  if (el == void 0) {
+    el = $("input");
+  }
+
+  el.each(function (i, el) {
+    var inp = $(el), param = {};
+
+    if (inp.attr('data-inputmask') != void 0) {
+      inp.inputmask();
+    }
+
+    if (inp.attr('data-inputmask-email') != void 0) {
+      param.regex = inp.attr('data-inputmask-email');
+      param.placeholder = '_';
+
+      inp.inputmask('Regex', param);
+    }
+
+    if (inp.attr('data-inputmask-regex') != void 0) {
+      inp.inputmask('Regex', param);
+    }
+
+    if (inp.attr('data-inputmask-custom') != void 0) {
+      inp.inputmask(JSON.parse('{' + inp.attr('data-inputmask-custom').replace(/'/g, '"') + '}'));
+    }
+
+  });
+}
 
 function toggleOverlay(target, show) {
   var ov = $('<div class="gl-overlay glOverlay"></div>');
@@ -94,6 +166,20 @@ function initHeroSlider() {
       pagination: '.heroSlider .b-hero__slider-pagination',
       prevButton: '.heroSlider .b-hero__slider-prev',
       nextButton: '.heroSlider .b-hero__slider-next'
+    });
+  }
+}
+
+function initMenuSlider() {
+
+  if ($('.menuSlider').length) {
+    menuSlider = new Swiper('.menuSlider .swiper-container', {
+      speed: 600,
+      spaceBetween: 0,
+      freeMode: !0,
+      slidesPerView: 'auto',
+      prevButton: '.menuSlider .b-header__menu-butt._prev',
+      nextButton: '.menuSlider .b-header__menu-butt._next'
     });
   }
 }
